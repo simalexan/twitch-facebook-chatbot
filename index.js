958cdccebaf0d2898fe12609d6849ebb9b77a5fe
Aleksandar Simovic <alexander.simovic@gmail.com>
2018-08-22 23:24:30 +0200

    adding NLP

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 'use strict';
-
-const botBuilder = require('claudia-bot-builder');
+const botBuilder = require('claudia-bot-builder'),
+  AWS = require('aws-sdk'),
+  comprehend = new AWS.Comprehend(),
+  SCHEDULE_REPLY = 'Hey, my streams are usually happenning on Monday, Tuesday, Wednesday and Thursday from 6pm.',
+  CHANNEL_REPLY = 'Thanks for asking! This is my Twitch channel - https://twitch.tv/simalexan';
 
 module.exports = botBuilder(function (message) {
 
-  if (message.text.includes('stream')) {
-    return `Hey, my streams are usually happenning on Monday, Tuesday, Wednesday and Thursday from 6pm.`;
-  }
+  let currentReply = '',
+    isScheduleInquiry = false, 
+    isChannelInquiry = false;
 
-  if (message.text.includes('twitch')) {
-    return `You can find me on twitch on https://twitch.tv/simalexan`;
-  }
-  
-  return message.text;
+  return comprehend.detectKeyPhrases({
+    LanguageCode: 'en',
+    Text: message.text
+  }).promise()
+    .then(response => {
+      console.log(response);
+      response.KeyPhrases.forEach(entity => {
+        if (!isScheduleInquiry) isScheduleInquiry = isScheduleDetected(entity.Text);
+        if (!isChannelInquiry) isChannelInquiry = isChannelDetected(entity.Text);
+      });
+
+      if (isScheduleInquiry) currentReply = SCHEDULE_REPLY;
+      if (!isScheduleInquiry && isChannelInquiry) currentReply = CHANNEL_REPLY;
+
+      return comprehend.detectSentiment({ LanguageCode: 'en', Text: message.text }).promise();
+    }).then(response => {
+      switch(response.Sentiment) {
+        case 'NEGATIVE':
+          currentReply += ` Sorry to hear that! Thank you, I'll try to improve!`
+          break;
+        case 'POSITIVE':
+          currentReply += ' Thank you for your kind words!';
+          break;
+        default:
+          break;
+      }
+      return currentReply;
+    });
 }, { platforms: ['facebook'] });
+
+function isScheduleDetected(text) {
+  return text.includes('schedule') || text.includes('time');
+}
+
+function isChannelDetected(text) {
+  return text.includes('channel') || text.includes('Twitch');
+}
